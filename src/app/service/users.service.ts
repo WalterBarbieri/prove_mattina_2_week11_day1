0f2205c1d95f04914885bb339556e62e20dcefb6
Walter Barbieri <128515233+uolli90@users.noreply.github.com>
2023-06-06 09:57:36 +0200

    1

--- src/app/service/users.service.ts
+++ src/app/service/users.service.ts
@@ -1,25 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs';
 import { Users } from '../interface/users.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UsersService {
-
   constructor(private http: HttpClient) {}
-    recupera() {
-      return this.http.get<{data: Users[]}>("https://reqres.in/api/users").pipe(map(risultato => risultato.data))
-    }
+  recupera() {
+    return this.http
+      .get<{ data: Users[] }>('https://reqres.in/api/users')
+      .pipe(map((risultato) => risultato.data));
+  }
 
-    nuovoUtente(newUser: Partial<Users>) {
-      return this.http.post<Users>("https://reqres.in/api/users", newUser)
-    }
+  nuovoUtente(newUser: Partial<Users>) {
+    return this.http.post<Users>('https://reqres.in/api/users', newUser);
+  }
 
-    cancella(id: number) {
-      return this.http.delete(`https://reqres.in/api/users/${id}`)
-    }
-
-
+  cancella(id: number) {
+    return this.http.delete(`https://reqres.in/api/users/${id}`);
+  }
 }
